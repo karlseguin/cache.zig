af1a100666aa9c10aa323618f76679f379df54b6
Karl Seguin <k@openmymind.io>
2024-01-06 09:45:39 +0800

    source_file -> root_source_file for breaking Zig changes

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) !void {
 	const optimize = b.standardOptimizeOption(.{});
 
 	_ = b.addModule("cache", .{
-		.source_file = .{ .path = "src/cache.zig" },
+		.root_source_file = .{ .path = "src/cache.zig" },
 	});
 
 	const lib_test = b.addTest(.{
